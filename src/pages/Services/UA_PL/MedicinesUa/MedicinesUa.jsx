db639132ad8f07b7fb77c6333d18cf340405e498
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-08-02 16:28:19 +0200

    npm run dev

--- src/pages/Services/UA_PL/MedicinesUa/MedicinesUa.jsx
+++ src/pages/Services/UA_PL/MedicinesUa/MedicinesUa.jsx
@@ -4,7 +4,20 @@ import FormMedicinesUA from "./FormMedicinesUA/FormMedicinesUA";
 import s from "./MedicinesUa.module.css";
 const MedicinesUa = () => {
   return (
-    <div className={s.cont}>
+    <div className={s.header}>
+      <div className={s.home}>
+        <h1 className={s.title}>Доставка посилок з України</h1>
+        <p className={s.description}>
+          документи, особисті речі, коштовні речі, вантажі для бізнесу тощо
+        </p>
+        <div className={s.iconplace}>
+          {/* <IoIosArrowDown
+                  className={s.icon}
+                  size={48}
+                  onClick={scrollToSection}
+                /> */}
+        </div>
+      </div>
       <FormMedicinesUA url={testUrl} />
     </div>
   );
